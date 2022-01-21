706803cf8ceb96511e6da3a69a1c5f226ea6e39f
Stuart Bradshaw <stuartb@Hyperion.local>
2022-01-21 19:52:53 +0000

    Add Firebase user sign in functionality

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,17 @@
+import { useEffect, useState } from "react";
+import { getAuth } from "firebase/auth";
+
 function Profile() {
-  return (
-    <div>
-      <h1>Profile</h1>
-    </div>
-  );
+  const [user, setUser] = useState(null);
+
+  const auth = getAuth();
+
+  useEffect(() => {
+    setUser(auth.currentUser);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return user ? <h1>{user.displayName}</h1> : "Not logged in...";
 }
 
 export default Profile;
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
 import visibilityIcon from "../assets/svg/visibilityIcon.svg";
 
@@ -21,13 +22,33 @@ function SignIn() {
     }));
   };
 
+  const onSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const auth = getAuth();
+
+      const userCredentials = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+
+      if (userCredentials.user) {
+        navigate("/");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
       <div className="pageContainer">
         <header>
           <p className="pageHeader">Welcome Back!</p>
         </header>
-        <form>
+        <form onSubmit={onSubmit}>
           <input
             id="email"
             type="email"

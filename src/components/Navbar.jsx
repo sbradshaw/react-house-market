530f3f0dc8458baf3ef0461f2ec2d06904030d9e
Stuart Bradshaw <stuartb@Hyperion.local>
2022-02-13 18:37:09 +0000

    Add offers page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -44,7 +44,7 @@ function Navbar() {
                   : "navbarListItemName"
               }
             >
-              Offer
+              Offers
             </p>
           </li>
           <li className="navbarListItem" onClick={() => navigate("/profile")}>
--- src/pages/Offers.jsx
+++ src/pages/Offers.jsx
@@ -1,7 +1,82 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import {
+  collection,
+  getDocs,
+  query,
+  where,
+  orderBy,
+  limit,
+  startAfter,
+  getDoc,
+} from "firebase/firestore";
+import { toast } from "react-toastify";
+import { db } from "../firebase.config";
+import Spinner from "../components/Spinner";
+import ListingItem from "../components/ListingItem";
+
 function Offers() {
+  const [listings, setListings] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const params = useParams();
+
+  useEffect(() => {
+    const fetchListings = async () => {
+      try {
+        const listingsReference = collection(db, "listings");
+
+        const q = query(
+          listingsReference,
+          where("offer", "==", true),
+          orderBy("timestamp", "desc"),
+          limit(10)
+        );
+
+        const querySnapshot = await getDocs(q);
+        const listings = [];
+
+        querySnapshot.forEach((doc) => {
+          return listings.push({
+            id: doc.id,
+            data: doc.data(),
+          });
+        });
+
+        setListings(listings);
+        setLoading(false);
+      } catch (error) {
+        toast.error("Could not fetch property listings");
+      }
+    };
+
+    fetchListings();
+  }, []);
+
   return (
-    <div>
-      <h1>Offers</h1>
+    <div className="category">
+      <header>
+        <p className="pageHeader">Offers</p>
+      </header>
+      {loading ? (
+        <Spinner />
+      ) : listings && listings.length > 0 ? (
+        <>
+          <main>
+            <ul className="categoryListings">
+              {listings.map((listing) => (
+                <ListingItem
+                  listing={listing.data}
+                  id={listing.id}
+                  key={listing.id}
+                />
+              ))}
+            </ul>
+          </main>
+        </>
+      ) : (
+        <p>There are no current offers</p>
+      )}
     </div>
   );
 }

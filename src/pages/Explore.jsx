4efec43e1f572124dd2181129cd39b0685d1625a
Stuart Bradshaw <stuartb@Hyperion.local>
2022-03-25 18:51:29 +0000

    Add an image slider to the explore page

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import Slider from "../components/Slider";
 import rentCategoryImage from "../assets/jpg/rentCategoryImage.jpg";
 import sellCategoryImage from "../assets/jpg/sellCategoryImage.jpg";
 
@@ -8,7 +9,9 @@ function Explore() {
       <header>
         <p className="pageHeader">Explore</p>
       </header>
-      <main>{/* Slider */}</main>
+      <main>
+        <Slider />
+      </main>
       <p className="exploreCategoryHeading">Categories</p>
       <div className="exploreCategories">
         <Link to="/category/rent">
